ef6c91920de9a9cc4129a8ea375baccfb1cc76e6
ligia <ligiaangelica23@gmail.com>
2026-05-12 02:07:51 -0500

    primer push

---
+++ src/main/java/com/xtremefitnesscenter/XtremeFitnessCenterApplication.java
@@ -0,0 +1,13 @@
+package com.xtremefitnesscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtremeFitnessCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtremeFitnessCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xtremefitnesscenter/XtremeFitnessCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtremefitnesscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtremeFitnessCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
